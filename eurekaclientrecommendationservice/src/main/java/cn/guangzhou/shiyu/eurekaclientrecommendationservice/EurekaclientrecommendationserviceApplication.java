e721443556a2919ddef11cde1ac70e233c4167df
shiyu546 <shiyu930416294@163.com>
2019-04-11 00:58:54 +0800

    Eureka服务端口不对

--- eurekaclientrecommendationservice/src/main/java/cn/guangzhou/shiyu/eurekaclientrecommendationservice/EurekaclientrecommendationserviceApplication.java
+++ eurekaclientrecommendationservice/src/main/java/cn/guangzhou/shiyu/eurekaclientrecommendationservice/EurekaclientrecommendationserviceApplication.java
@@ -3,24 +3,26 @@ package cn.guangzhou.shiyu.eurekaclientrecommendationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@ComponentScan(excludeFilters = {@ComponentScan.Filter(type= FilterType.ANNOTATION,value = ExcludeFromComponetScan.class)})
+@EnableEurekaClient
 @RibbonClient(name="say-Hello",configuration = SayHelloConfiguration.class)
 public class EurekaclientrecommendationserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaclientrecommendationserviceApplication.class, args);
-
-	}
-
-	@LoadBalanced
 	@Bean
+	@LoadBalanced
 	RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaclientrecommendationserviceApplication.class, args);
+	}
+
+
 
 }
